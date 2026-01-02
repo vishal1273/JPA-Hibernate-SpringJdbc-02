528d92dcd0c3478ad0d3637ed0a5394e20736d7d
Vishal Thakur <vishalthakur107@gmail.com>
2026-01-02 08:47:27 +0530

    Querying Data using JPA and Entity Manager

--- src/main/java/com/vishal/springboot/jpa/model/Course.java
+++ src/main/java/com/vishal/springboot/jpa/model/Course.java
@@ -1,5 +1,7 @@
 package com.vishal.springboot.jpa.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +9,9 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Course {
+    @Id
     private long id;
     private String name;
     private String author;
